c3548a127c994a2817196afda88a9a613ea593f7
Nhico Noel Ortazon <legendarynhico@gmail.com>
2025-08-20 17:00:03 +0800

    Add project files.

---
+++ Smart Expenses V2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Smart_Expenses_V2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
