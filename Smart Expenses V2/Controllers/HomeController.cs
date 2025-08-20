707feab4526b7fd4d35b26dc0598d1fa091e24ce
Nhico Noel Ortazon <legendarynhico@gmail.com>
2025-08-20 17:12:18 +0800

    Adding Basic Page

--- Smart Expenses V2/Controllers/HomeController.cs
+++ Smart Expenses V2/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Smart_Expenses_V2.Controllers
             return View();
         }
 
+        public IActionResult Expenses()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Smart Expenses V2/Models/Expenses.cs
@@ -0,0 +1,12 @@
+﻿namespace Smart_Expenses_V2.Models
+{
+    public class Expenses
+    {
+        public int Id { get; set; }
+
+        public decimal Value { get; set; }
+
+        public string? Description { get; set; }
+
+    }
+}
